585819d38a5d273251a9ac0953d0a5988a595c45
kyab <kyossi212@gmail.com>
2014-10-05 23:38:29 +0900

    Initial Commit

---
+++ mruby-mirb-server/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  mruby-mirb-server
+//
+//  Created by koji on 2014/10/05.
+//  Copyright (c) 2014年 kyab. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
